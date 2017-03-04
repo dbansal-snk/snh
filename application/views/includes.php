d9ad177e3d1b9b62a4fe149b3130ff0207a4eea9
dbansal-snk <deepak@snakindia.com>
2017-03-04 20:48:51 +0530

    Added js

--- application/views/includes.php
+++ application/views/includes.php
@@ -1,28 +1,25 @@
 <link rel="stylesheet" href="<?php echo base_url();?>template/css/font.css">
 
-<link href="<?php echo base_url();?>template/bootstrap-sweetalert-master/dist/sweetalert.css" media="screen" rel="stylesheet" type="text/css" />
+<!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800">-->
 <link href="<?php echo base_url();?>template/css/bayanno.css" media="screen" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url();?>template/css/jquery.dataTables.css" media="screen" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url();?>template/css/snh.css" media="screen" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url();?>template/css/jquery-ui.css" media="screen" rel="stylesheet" type="text/css" />
- <link href="<?php echo base_url();?>template/css/select2/select2.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url();?>template/bootstrap-sweetalert-master/dist/sweetalert.css" media="screen" rel="stylesheet" type="text/css" />
 
 <!--[if lt IE 9]>
 <script src="<?php echo base_url();?>template/js/html5shiv.js" type="text/javascript"></script>
 
 <script src="<?php echo base_url();?>template/js/excanvas.js" type="text/javascript"></script>
 <![endif]-->
+<script src="<?php echo base_url();?>template/bootstrap-sweetalert-master/dist/sweetalert.min.js" type="text/javascript"></script>
 
-
-
-<script src="<?php echo base_url();?>template/js/datatable/ReportTable.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>template/js/jquery-3.1.1.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url();?>template/js/bayanno.js" type="text/javascript"></script>
 <script src="<?php echo base_url();?>template/js/jquery-ui.js" type="text/javascript"></script>
 <script src="<?php echo base_url();?>template/js/snh_common.js" type="text/javascript"></script>
 <script src="<?php echo base_url();?>template/js/jquery.dataTables.js" type="text/javascript"></script>
-<script src="<?php echo base_url();?>template/bootstrap-sweetalert-master/dist/sweetalert.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url();?>template/js/jquery-3.1.1.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url();?>template/js/select2/select2.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>template/js/datatable/ReportTable.js" type="text/javascript"></script>
 
 <?php
 
